19f12f9c247fb00b95bbe6f0d195c3470a286a95
Jacob Ras <jacobras.nl@gmail.com>
2026-09-23 22:19:19 +0200

    Add wasmJs target to [design]

--- design/build.gradle.kts
+++ design/build.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("OPT_IN_USAGE")
+
 plugins {
     kotlin("multiplatform")
     alias(libs.plugins.android.kmp.library)
@@ -15,14 +17,17 @@ kotlin {
             enable = true
         }
     }
+    wasmJs { browser() }
 
     sourceSets {
-        androidMain.dependencies {
+        commonMain.dependencies {
             implementation(libs.compose.foundation)
             implementation(libs.compose.material3)
             implementation(libs.compose.ui)
-            implementation(libs.compose.ui.tooling)
             implementation(libs.compose.ui.tooling.preview)
+        }
+        androidMain.dependencies {
+            implementation(libs.compose.ui.tooling)
             implementation(libs.lottie)
         }
     }
