472fc4b5ddb4fa8eff0d693074d5aad271c50c2e
Jacob Ras <jacobras.nl@gmail.com>
2026-09-23 22:19:32 +0200

    Commonise `Theme`

--- design/src/commonMain/kotlin/com/example/util/design/foundation/Theme.kt
+++ design/src/commonMain/kotlin/com/example/util/design/foundation/Theme.kt
@@ -1,29 +1,17 @@
 package com.example.util.design.foundation
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun SpecialTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = getColorScheme(darkTheme = darkTheme)
 
     MaterialTheme(
         colorScheme = colorScheme,
@@ -32,13 +20,13 @@ fun SpecialTheme(
     )
 }
 
-private val DarkColorScheme = darkColorScheme(
+internal val DarkColorScheme = darkColorScheme(
     primary = Blue80,
     secondary = BlueGrey80,
     tertiary = Sky80
 )
 
-private val LightColorScheme = lightColorScheme(
+internal val LightColorScheme = lightColorScheme(
     primary = Blue40,
     secondary = BlueGrey40,
     tertiary = Sky40
