912ba3d692de4e6cbd00ed27b60691cc943949d5
Jacob Ras <jacobras.nl@gmail.com>
2026-09-23 22:10:24 +0200

    Remove unused boolean

--- design/src/main/kotlin/com/example/util/design/foundation/Theme.kt
+++ design/src/main/kotlin/com/example/util/design/foundation/Theme.kt
@@ -13,12 +13,10 @@ import androidx.compose.ui.platform.LocalContext
 @Composable
 fun SpecialTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
