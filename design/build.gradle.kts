68d07d359a8a4e1dc1ca4361047b263ea3c7dc09
Jacob Ras <jacobras.nl@gmail.com>
2026-09-23 21:16:18 +0200

    Migrate [design] to a KMP build script

--- design/build.gradle.kts
+++ design/build.gradle.kts
@@ -1,24 +1,29 @@
 plugins {
-    alias(libs.plugins.android.library)
+    kotlin("multiplatform")
+    alias(libs.plugins.android.kmp.library)
     alias(libs.plugins.kotlin.compose)
 }
 
-android {
-    namespace = "nl.jacobras.design"
-    compileSdk {
-        version = release(37)
+kotlin {
+    android {
+        namespace = "nl.jacobras.design"
+        compileSdk {
+            version = release(37)
+        }
+        androidResources {
+            enable = true
+        }
     }
-    buildFeatures {
-        compose = true
-    }
-}
 
-dependencies {
-    implementation(platform(libs.compose.bom))
-    implementation(libs.compose.foundation)
-    implementation(libs.compose.material3)
-    implementation(libs.compose.ui)
-    implementation(libs.compose.ui.tooling)
-    implementation(libs.compose.ui.tooling.preview)
-    implementation(libs.lottie)
+    sourceSets {
+        androidMain.dependencies {
+            implementation(project.dependencies.platform(libs.compose.bom))
+            implementation(libs.compose.foundation)
+            implementation(libs.compose.material3)
+            implementation(libs.compose.ui)
+            implementation(libs.compose.ui.tooling)
+            implementation(libs.compose.ui.tooling.preview)
+            implementation(libs.lottie)
+        }
+    }
 }
