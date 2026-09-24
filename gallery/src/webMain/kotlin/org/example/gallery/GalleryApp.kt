570dedc65d2b4eb76915e7d7b07b9ca79af44899
Jacob Ras <jacobras.nl@gmail.com>
2026-09-24 23:40:07 +0200

    Add `Screen` enum

--- gallery/src/webMain/kotlin/org/example/gallery/GalleryApp.kt
+++ gallery/src/webMain/kotlin/org/example/gallery/GalleryApp.kt
@@ -1,17 +1,23 @@
 package org.example.gallery
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffold
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
 import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.util.design.foundation.SpecialTheme
-import org.example.gallery.playgrounds.CardPlayground
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3AdaptiveApi::class)
 @Composable
@@ -24,17 +30,40 @@ internal fun GalleryApp() {
                 )
             }
         ) { paddingValues ->
-            val navigator = rememberListDetailPaneScaffoldNavigator()
+            val navigator = rememberListDetailPaneScaffoldNavigator<Screen>()
+            val scope = rememberCoroutineScope()
 
             Box(modifier = Modifier.padding(paddingValues)) {
                 ListDetailPaneScaffold(
                     directive = navigator.scaffoldDirective,
                     value = navigator.scaffoldValue,
                     listPane = {
-                        Text("List...")
+                        Column {
+                            for (screen in Screen.entries) {
+                                Text(
+                                    text = screen.title,
+                                    modifier = Modifier.clickable {
+                                        scope.launch {
+                                            navigator.navigateTo(
+                                                pane = ListDetailPaneScaffoldRole.Detail,
+                                                contentKey = screen
+                                            )
+                                        }
+                                    }
+                                )
+                            }
+                        }
                     },
                     detailPane = {
-                        CardPlayground()
+                        val screen = navigator.currentDestination?.contentKey
+                        if (screen != null) {
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.TopCenter
+                            ) {
+                                screen.content()
+                            }
+                        }
                     }
                 )
             }
