86ff9e47203628913001d3d3ec9c87e9a0cf8440
Jacob Ras <jacobras.nl@gmail.com>
2026-09-25 14:57:22 +0200

    Fix `SpecialButton`'s dark theme & enabled/disabled state

--- design/src/commonMain/kotlin/com/example/util/design/components/SpecialButton.kt
+++ design/src/commonMain/kotlin/com/example/util/design/components/SpecialButton.kt
@@ -3,6 +3,7 @@ package com.example.util.design.components
 import androidx.compose.foundation.border
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -25,7 +26,11 @@ fun SpecialButton(
         ),
         modifier = Modifier.border(
             width = 5.dp,
-            color = Color.Blue.copy(red = 0.3f, green = 0.3f),
+            color = if (enabled) {
+                Color.Blue.copy(red = 0.3f, green = 0.3f)
+            } else {
+                Color.Gray
+            },
             shape = ButtonDefaults.shape
         ),
         enabled = enabled,
@@ -34,15 +39,44 @@ fun SpecialButton(
         Text(
             text = label,
             fontStyle = FontStyle.Italic,
-            fontWeight = FontWeight.ExtraBold
+            fontWeight = FontWeight.ExtraBold,
+            color = if (enabled) {
+                Color.White
+            } else {
+                LocalContentColor.current
+            }
         )
     }
 }
 
 @Preview
 @Composable
-private fun Preview() {
-    SpecialTheme {
+private fun PreviewLight() {
+    SpecialTheme(darkTheme = false) {
         SpecialButton("Click me", onClick = {})
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewLightDisabled() {
+    SpecialTheme(darkTheme = false) {
+        SpecialButton("Click me", enabled = false, onClick = {})
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewDark() {
+    SpecialTheme(darkTheme = true) {
+        SpecialButton("Click me", onClick = {})
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewDarkDisabled() {
+    SpecialTheme(darkTheme = true) {
+        SpecialButton("Click me", enabled = false, onClick = {})
     }
 }
